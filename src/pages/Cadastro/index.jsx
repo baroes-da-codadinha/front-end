02d194fef9694afa839332570e17f3d97cd36f6b
RODRIGO NOAL <rodrigonoal@gmail.com>
2021-07-27 20:31:13 -0300

    tarefa: página de cadastro, parte visual

--- src/pages/Cadastro/index.jsx
+++ src/pages/Cadastro/index.jsx
@@ -1,45 +1,151 @@
+/* eslint-disable no-param-reassign */
 /* eslint-disable jsx-a11y/label-has-associated-control */
 /* eslint-disable import/no-named-as-default-member */
 /* eslint-disable import/no-named-as-default */
 import React, { useState } from 'react';
 import './styles.css';
 import { NavLink } from 'react-router-dom';
-import Illustration from '../../assets/illustration.svg';
-import IllustrationTwo from '../../assets/illustration-2.svg';
 import InputSenha from '../../components/InputSenha';
+import InputTexto from '../../components/InputTexto';
+import Textarea from '../../components/Textarea';
+import InputValor from '../../components/InputValor';
+import Stepper from '../../components/Stepper';
 
 export default function Cadastro() {
+  const [step, setStep] = useState([{
+    valor: '1',
+    status: 'editando',
+  }, {
+    valor: '2',
+    status: '',
+  }, {
+    valor: '3',
+    status: '',
+  },
+  ]);
+  const [nome, setNome] = useState('');
+  const [email, setEmail] = useState('');
   const [senha, setSenha] = useState('');
   const [confirmarSenha, setConfirmarSenha] = useState('');
+  const [descricao, setDescricao] = useState('');
+
+  function handleIr() {
+    const newStep = [...step];
+    for (let i = 0; i < newStep.length; i++) {
+      if (newStep[i].status === 'editando') {
+        newStep[i].status = 'concluido';
+      }
+      if (newStep[i].status === '') {
+        newStep[i].status = 'editando';
+        break;
+      }
+    }
+    setStep(newStep);
+  }
+
+  function handleVoltar() {
+    const newStep = [...step];
+    for (let i = 2; i >= 0; i--) {
+      if (newStep[i].status === 'editando') {
+        newStep[i].status = '';
+      }
+      if (newStep[i].status === 'concluido') {
+        newStep[i].status = 'editando';
+        break;
+      }
+    }
+    setStep(newStep);
+  }
 
   return (
     <div className="img-cadastro">
-      <img className="ilustracao 1" src={Illustration} alt="" />
-      <img className="ilustracao 2" src={IllustrationTwo} alt="" />
       <div className="base cadastro">
-        <span className="titulo pagina">Cadastro</span>
+        <div className="title-box">
+          <span className="titulo pagina">Cadastro</span>
+          <Stepper step={step} />
+        </div>
         <form className="formulario">
-          <div className="flex-column">
-            <label htmlFor="nome">Nome de usuário</label>
-            <input id="nome" type="text" />
-          </div>
-          <div className="flex-column">
-            <label htmlFor="email">Email</label>
-            <input id="email" type="text" />
-          </div>
-          <InputSenha
-            label="Senha"
-            value={senha}
-            setValue={setSenha}
-          />
-          <InputSenha
-            label="Repita a senha"
-            value={confirmarSenha}
-            setValue={setConfirmarSenha}
-          />
+          {step[0].status === 'editando' && (
+            <>
+              <InputTexto
+                label="Nome do usuário"
+                value={nome}
+                setValue={setNome}
+              />
+              <InputTexto
+                label="Email"
+                value={email}
+                setValue={setEmail}
+              />
+              <InputSenha
+                label="Senha"
+                value={senha}
+                setValue={setSenha}
+              />
+              <InputSenha
+                label="Repita a senha"
+                value={confirmarSenha}
+                setValue={setConfirmarSenha}
+              />
+            </>
+          )}
+          {step[1].status === 'editando' && (
+            <>
+              <InputTexto
+                label="Nome do restaurante"
+              />
+              <InputTexto
+                label="Categoria do restaurante"
+              />
+              <Textarea
+                label="Descrição"
+                maxLength="50"
+                value={descricao}
+                setValue={setDescricao}
+              />
+            </>
+          )}
+          {step[2].status === 'editando' && (
+            <>
+              <InputValor
+                label="Taxa de entrega"
+              />
+              <InputTexto
+                label="Tempo estimado de entrega"
+              />
+              <InputValor
+                label="Valor mínimo do pedido"
+              />
+            </>
+          )}
           <div className="button-box">
-            <button className="cancelar" type="button">Anterior</button>
-            <button className="aceitar" type="submit">Próximo</button>
+            <button
+              className="cancelar"
+              type="button"
+              onClick={() => handleVoltar()}
+              disabled={!step[1].status}
+            >
+              Anterior
+            </button>
+            {step[2].status ? (
+              <button
+                className="aceitar"
+                type="button"
+                onClick={() => handleIr()}
+                disabled
+              >
+                Criar conta
+              </button>
+            ) : (
+              <button
+                className="aceitar"
+                type="button"
+                onClick={() => handleIr()}
+              >
+                Próximo
+              </button>
+            )}
+
           </div>
           <div className="link-box">
             <span>Já tem uma conta? </span>
@@ -47,6 +153,7 @@ export default function Cadastro() {
           </div>
         </form>
       </div>
+      <div className="ilustracao" />
     </div>
   );
 }
