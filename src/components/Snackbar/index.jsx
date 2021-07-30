9d6c3cf9887458757606448d238d9c4e69625188
RODRIGO NOAL <rodrigonoal@gmail.com>
2021-07-30 15:43:00 -0300

    tarefa: correcao de bug e novos ícones

--- src/components/Snackbar/index.jsx
+++ src/components/Snackbar/index.jsx
@@ -3,6 +3,7 @@
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 import React, { useState } from 'react';
 import './styles.css';
+import WarnIcon from '../../assets/warning.svg';
 
 export default function Snackbar({ openSnack, setOpenSnack }) {
   const [testeMensagem, setTestMensagem] = useState({
@@ -18,7 +19,10 @@ export default function Snackbar({ openSnack, setOpenSnack }) {
         {openSnack && (
           <>
             <div className="icone-alerta">
-              !
+              <img
+                src={WarnIcon}
+                alt=""
+              />
             </div>
             <span>
               {testeMensagem.mensagem}
